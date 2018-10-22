a94e5f762535d56f547b185846512cadacbe6015
andmarti1424 <andmarti@gmail.com>
2018-10-22 20:11:34 -0300

    .

--- sheet.h
+++ sheet.h
@@ -2,6 +2,7 @@ int coltoa(int col, char * rname);
 int convert(int * col, int * row, char * s, int size);
 struct Sheet * Search_sheet(struct roman *doc, char *name);
 struct Sheet * new_sheet(struct roman * doc, char * name);
+void delete_sheet(struct roman *doc, struct Sheet *sh);
 struct Ent * lookat(struct Sheet * sh, int row, int col);
 
 int growtbl(struct Sheet * sh, int rowcol, int toprow, int topcol);
